d53ac2a31b3569ab2fd61c20a9bd9fb15b8f3877
B20DCAT186TranDangThuc <trthucpro@gmail.com>
2024-10-11 09:28:59 +0700

    video 25

---
+++ src/main/java/com/dangthuc/job/springrestfulmaven/SpringRestfulMavenApplication.java
@@ -0,0 +1,18 @@
+package com.dangthuc.job.springrestfulmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+
+public class SpringRestfulMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestfulMavenApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/dangthuc/job/springrestfulmaven/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.dangthuc.job.springrestfulmaven.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> getHelloWorld() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
