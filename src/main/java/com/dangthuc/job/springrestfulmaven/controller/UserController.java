c6f4c52be5e4db09aa537d17cd9dca2aa3cce018
B20DCAT186TranDangThuc <trthucpro@gmail.com>
2024-10-11 10:07:24 +0700

    video 35

--- src/main/java/com/dangthuc/job/springrestfulmaven/controller/UserController.java
+++ src/main/java/com/dangthuc/job/springrestfulmaven/controller/UserController.java
@@ -5,6 +5,8 @@ import com.dangthuc.job.springrestfulmaven.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserController {
@@ -15,14 +17,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
-    public ResponseEntity<User> create() {
+    @PostMapping("/user/create")
+    public ResponseEntity<User> create(@RequestBody User request) {
 
-        User user = new User();
-        user.setName("Dang Thuc");
-        user.setEmail("thuc@gmail.com");
-        user.setPassword("Dang Thuc");
-
-        return ResponseEntity.ok(userService.createUser(user));
+        return ResponseEntity.ok(userService.createUser(request));
     }
 }
